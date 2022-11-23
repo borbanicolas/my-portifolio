33790ebf185eac46798faf16f7aebd4e124c1355
borbanicolas <borbanicolasmk@gmail.com>
2022-11-23 20:15:09 -0300

    trabalhando no too-bar

--- configs/components/menu/menu.js
+++ configs/components/menu/menu.js
@@ -1,10 +1,13 @@
 const menuBtn = document.querySelector('[data-menu-btn]');
 const menuList = document.querySelector('[data-menu-list]');
 const bannerInfo = document.querySelector('[data-banner-info]')
+const tooBarIcons = document.querySelector('[data-too-bar-icon]')
 
 menuBtn.addEventListener('click',() => {
     menuBtn.classList.toggle('open')
     menuList.classList.toggle('open-list')
     bannerInfo.classList.toggle('hide-info')
+    tooBarIcons.classList.toggle('hide-icon-too-bar')
+    
 });
 
